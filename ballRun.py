06d698c0ac1fa7e5c01ffd251100924009f97667
Caleb Brian <caleb.brian@connect.qut.edu.au>
2018-05-17 23:47:13 +0000

    Changes to be committed: 	modified:   MFRC522.pyc 	modified:   SimpleMFRC522.pyc 	modified:   ballRun.py
     Untracked files:
    	LIS3DH.pyc

--- ballRun.py
+++ ballRun.py
@@ -1,6 +1,6 @@
 """Main program to run the Ball"""
 import sys
-import Rpi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import SimpleMFRC522
 from LIS3DH import LIS3DH
 from time import sleep
@@ -8,17 +8,15 @@ from time import sleep
 def main():
 	"""Main entry point for the program"""
 	sensor = LIS3DH()
-    sensor.setRange(LIS3DH.RANGE_2G)
+	sensor.setRange(LIS3DH.RANGE_2G)
 
 	while True:
 		x = abs(sensor.getX())
-        y = abs(sensor.getY())
-        z = abs(sensor.getZ())
-        average = (x+y+z)/3
-        print(average)
-        sleep(0.1)
-
-
+	        y = abs(sensor.getY())
+	        z = abs(sensor.getZ())
+	        average = (x+y+z)/3
+	        print(average)
+	        sleep(0.1)
 
 if __name__ == '__main__':
 	sys.exit(main())
