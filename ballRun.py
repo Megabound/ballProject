711d71372f0ebb1fb72c079c16b2182348674cf0
Caleb Brian <caleb.brian@connect.qut.edu.au>
2018-05-18 00:38:47 +0000

    RFID Scanner and Accel Working

--- ballRun.py
+++ ballRun.py
@@ -5,17 +5,31 @@ import SimpleMFRC522
 from LIS3DH import LIS3DH
 from time import sleep
 
+def readAccel(sensor):
+	x = abs(sensor.getX())
+	y = abs(sensor.getY())
+	z = abs(sensor.getZ())
+	average = (x+y+z)/3
+	return average
+
+def readRFID(reader):
+	id = 0
+	id = reader.read_id_no_block()
+	return id
+
 def main():
 	"""Main entry point for the program"""
 	sensor = LIS3DH()
 	sensor.setRange(LIS3DH.RANGE_2G)
+	reader = SimpleMFRC522.SimpleMFRC522()
 
 	while True:
-		x = abs(sensor.getX())
-	        y = abs(sensor.getY())
-	        z = abs(sensor.getZ())
-	        average = (x+y+z)/3
-	        print(average)
+		average = readAccel(sensor)
+		RFIDTag = readRFID(reader)
+		if average>0.6:
+			print("Caught!")
+		if RFIDTag != None:
+			print(RFIDTag)
 	        sleep(0.1)
 
 if __name__ == '__main__':
