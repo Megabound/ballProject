137a4b6e9794d4d3f42e22fd6a6bd4eceba063ec
Megabound <brian.caleb@gmail.com>
2018-06-08 13:49:02 +1000

    Update accelTest.py

--- accelTest.py
+++ accelTest.py
@@ -5,15 +5,13 @@ from LIS3DH import LIS3DH
 from time import sleep
 
 def main():
-	
-  sensor = LIS3DH()
+	sensor = LIS3DH()
 	sensor.setRange(LIS3DH.RANGE_2G)
-  
-  while True:
-    x = sensor.getX()
-    y = sensor.getY()
-    z = sensor.getZ()
-    print("X = "+x)
-    print("Y = "+y)
-    print("Z = "+z)
-    sleep(0.5)
+	while True:
+		x = sensor.getX()
+		y = sensor.getY()
+		z = sensor.getZ()
+		print("X = "+x)
+		print("Y = "+y)
+		print("Z = "+z)
+		sleep(0.5)
