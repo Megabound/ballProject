14dfa10a36cc0efc76dcdbf23fff178db35f48a1
Megabound <caleb.brian@connect.qut.edu.au>
2018-06-08 11:58:16 +1000

    Update ballRun.py

--- ballRun.py
+++ ballRun.py
@@ -1,13 +1,14 @@
-"""Main program to run the Ball"""
+#Main program to run the Ball
+
 import sys
 import RPi.GPIO as GPIO
 import SimpleMFRC522
-import vlc
+import pyglet
 import glob
 from LIS3DH import LIS3DH
 from time import sleep
 
-def readAccel(sensor): #Reads the data from the accelerometre
+def readAccel(sensor): #Reads the data from the accelerometer
 	x = abs(sensor.getX())
 	y = abs(sensor.getY())
 	z = abs(sensor.getZ())
@@ -20,8 +21,10 @@ def readRFID(reader): #Reads the data from the RFID tag and returns the text sto
 	id, text = reader.read_no_block()
 	return text
 
-def fileList(directory): #Returns a list of files in the specified directory
+def fileList(directory): #Returns a list of files in the specified directory, in the correct format for Pyglet
 	list = glob.glob("./" + directory + "/*")
+	for i in range(0,len(list)):
+		list[i]=list[i][4:]
 	return list, 0
 
 def updateIndex(list, index): #Updates the playlist index
@@ -31,30 +34,44 @@ def updateIndex(list, index): #Updates the playlist index
 	return index
 
 def main():
-	"""Main entry point for the program"""
+	
+	#User Configuration#
+	throwThreshold = 0.7 #Sensitivity for catch detaction, larger number = less sensitive
+	catchRepeat=3 #Number of times the ball should repeat a word in catch mode
+	tagRepeat=0 #Number of times the ball should repeat a word in RFID mode
+	#End user configuration
+
+	pyglet.options["audio"] = ('openal', 'pulse', 'directsound','silent')
+	catchCount=0
+	tagCount=0
 	sensor = LIS3DH()
 	sensor.setRange(LIS3DH.RANGE_2G)
 	reader = SimpleMFRC522.SimpleMFRC522()
 
 	rfidList, rfidIndex = fileList("./rfidSounds")
 	catchList, catchIndex = fileList("./catchSounds")
-	print(catchList)
-	print(catchIndex)
 
 	while True:
 		average = readAccel(sensor)
 		RFIDTag = readRFID(reader)
-		if average>0.6:
-			player = vlc.MediaPlayer(catchList[catchIndex])
-			player.play()
+		if average>throwThreshold:
+			pyPlayer = pyglet.resource.media(catchList[catchIndex])
+			pyPlayer.play()
 			print("Caught!")
-			catchIndex = updateIndex(catchList, catchIndex)
+			sleep(3)
+			catchCount+=1
+			if catchCount == catchRepeat:
+				catchCount=0
+				catchIndex = updateIndex(catchList, catchIndex)
 		if RFIDTag != None:
-			player = vlc.MediaPlayer(rfidList[rfidIndex])
-			player.play()
+			pyPlayer = pyglet.resource.media(rfidList[rfidIndex])
+			pyPlayer.play()
 			print(RFIDTag)
-			rfidIndex = updateIndex(rfidList,rfidIndex)
-	        sleep(0.1)
+			tagCount+=1
+			if tagCount == tagRepeat:
+				tagCount=0
+				rfidIndex = updateIndex(rfidList,rfidIndex)
+		sleep(0.1)
 
 if __name__ == '__main__':
 	sys.exit(main())
