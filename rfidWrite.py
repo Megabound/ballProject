c7e70b422ceabdee1b73bcbd909881e6837030df
Megabound <brian.caleb@gmail.com>
2018-06-08 13:37:11 +1000

    Update rfidWrite.py

--- rfidWrite.py
+++ rfidWrite.py
@@ -6,7 +6,7 @@ import SimpleMFRC522
 reader = SimpleMFRC522.SimpleMFRC522()
 
 try:
-  text = raw_input('New data:')
+  text = raw_input('New data: ')
   print("Now place your tag to write")
   reader.write(text)
   print("Written")
